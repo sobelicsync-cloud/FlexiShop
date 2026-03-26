d5528b5ae768b38717ed8eb44a95ea0114d26d82
sobelicsync-cloud <sobelicsync@gmail.com>
2026-03-25 21:10:16 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const STORE_CONFIG = {
+  name: "Flexishop",
+  whatsapp: "573000000000", // Cambia esto por tu número (Código país + número)
+  email: "contacto@flexishop.com",
+  address: "Calle Ficticia 123, Ciudad Moda",
+  currency: "$",
+  shippingCost: 10000,
+  categories: ["Vapes", "Gorras", "Busos", "Camisas", "Pantalones", "Pantalonetas", "Zapatos", "Lociones", "Gafas"]
+};
+
